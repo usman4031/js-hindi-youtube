6cfdb135dd4e01c85c018ab795eea4e8a7e48859
usman4031 <mo.usman4031@gmail.com>
2025-01-17 15:23:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"  // treat all js code as newer version
+
+// alert(3+3)   we are using nodejs, not browser
+
+
+console.log(3 + 4 );
+
+
+
+let name = "usman"
+let age  = 18
+let isLoggedIn = true
+//number
+// bigint
+// string ->  ""
+//boolean ->  true/ false
+// null -> standalone value     -> it is an object
+// undefined
+// symbol ->  unique 
+
+
